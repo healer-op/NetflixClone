0cb6504d12593297fee741d0e3272381c596dd29
HEALER <65026164+healer-op@users.noreply.github.com>
2021-05-26 16:01:46 +0530

    Updates

---
+++ precache-manifest.afcf501fe67ab7b672f80f6b166255d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05876abb27e8879251b499a9b82190a4",
+    "url": "/NetflixClone/index.html"
+  },
+  {
+    "revision": "b1841b48bbc7f65bfe92",
+    "url": "/NetflixClone/static/css/main.ef2b3607.chunk.css"
+  },
+  {
+    "revision": "53282a7f4210c98fd65c",
+    "url": "/NetflixClone/static/js/2.754e7dce.chunk.js"
+  },
+  {
+    "revision": "7e9a586c4d29df81a0106ce0dcaf059d",
+    "url": "/NetflixClone/static/js/2.754e7dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1841b48bbc7f65bfe92",
+    "url": "/NetflixClone/static/js/main.d9992da6.chunk.js"
+  },
+  {
+    "revision": "4e821e734f9e9aac872e",
+    "url": "/NetflixClone/static/js/runtime-main.cb66b7ab.js"
+  }
+]);
